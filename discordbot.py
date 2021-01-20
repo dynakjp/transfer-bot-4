77fad7008d53bb47682ee59b4e1726f5511281c4
dynakjp <77594764+dynakjp@users.noreply.github.com>
2021-01-20 18:54:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,9 @@
 import discord
 import os
 
+
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'DISCORD_BOT_TOKEN'
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 CID = 725325208537530370
@@ -13,16 +14,32 @@ async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    #条件に当てはまるメッセージかチェックし正しい場合は返す
-    def check(msg):
-        return msg.author == message.author
+# # メッセージ受信時に動作する処理
+# @client.event
+# async def on_message(message):
+#     #条件に当てはまるメッセージかチェックし正しい場合は返す
+#     def check(msg):
+#         return msg.author == message.author
     
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
+#     # メッセージ送信者がBotだった場合は無視する
+#     if message.author.bot:
+#         return
     
+    
+#     if message.content == '/here to':
+#         await message.channel.send('ここが送信元!送信先を教えてね！')
+#         wait_message=await client.wait_for("message",check=check)
+#         CID=int(wait_message.content)
+#         channel = client.get_channel(CID)
+#         await channel.send('送信先として設定')
+#         copy_message=wait_message
+#         while copy_message.content != '/fin':
+#             copy_message=await client.wait_for("message",check=check)
+#             if copy_message.channel==message.channel:
+#                 await channel.send("[{0}]{1}\n{2}".format(copy_message.channel.name,copy_message.author.name,copy_message.content))
+#         await message.channel.send('終了しました')
+#         await channel.send('終了しました')
+
+
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
